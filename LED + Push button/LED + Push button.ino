e63bf8c26bb15be9a5455bad3287b832787d4f6f
Loukastor <156593512+DLskell@users.noreply.github.com>
2024-01-24 21:34:18 +0100

    Update LED + Push button.ino

--- LED + Push button/LED + Push button.ino
+++ LED + Push button/LED + Push button.ino
@@ -1,47 +1,49 @@
-int ld1 = 5;
-int ld2 = 6;
-int ld3 = 7;
-int btn1 = A0;
-int btn2 = A2;
-int btn3 = A4;
+int ld1 = 5; //The first LED connected to port 5
+int ld2 = 6; //The second LED connected to port 6
+int ld3 = 7; //The third LED connected to port 7
+int btn1 = A0; //The first push button connected to port A0
+int btn2 = A2; //The second push button connected to port A2
+int btn3 = A4; //The third push button connected to port A4
 
 void setup() {
   pinMode(ld1, OUTPUT);
   pinMode(ld2, OUTPUT);
   pinMode(ld3, OUTPUT);
-  Serial.begin(9600);
+  pinMode(btn1, INPUT);
+  pinMode(btn2, INPUT);
+  pinMode(btn3, INPUT);
 }
 
 void loop(){
-  allumerdiode();
-  if(analogRead(btn1) > 200){
-    eteindrediode();
+  allumerdiode(); //The LEDs are all lit, waiting for a request
+  if(analogRead(btn1) > 200){ //The first push button is pressed
+    eteindrediode(); //First LED activation mode
     mode1();
     mode1();
     mode1();
   }
-  if(analogRead(btn2) > 200){
-    eteindrediode();
+  if(analogRead(btn2) > 200){ //The second push button is pressed
+    eteindrediode(); //Second LED activation mode
     mode2();
     mode2();
     mode2();
     mode2();
   }
-  if(analogRead(btn3) > 200){
-    eteindrediode();
+  if(analogRead(btn3) > 200){ //The push button 3 is pressed
+    eteindrediode(); //Third LED activation mode
     mode3();
     mode3();
     mode3();
   }
 }
 
-void allumerdiode(){
+void allumerdiode(){ //The LEDs are all lit
   digitalWrite(ld1, HIGH);
   digitalWrite(ld2, HIGH);
   digitalWrite(ld3, HIGH);
 }
 
-void eteindrediode(){
+void eteindrediode(){ //The LEDs are all off
   digitalWrite(ld1, LOW);
   digitalWrite(ld2, LOW);
   digitalWrite(ld3, LOW);
